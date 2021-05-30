742ab7fb0e10b594a5b5197581ff47f8b4dd9ed6
Erlend Eriksen <erlend_ne@hotmail.com>
2021-05-30 13:15:00 +0200

    ipc: merge python tests into c++ tests

--- tests/ipc/ipc.cpp
+++ tests/ipc/ipc.cpp
@@ -2,41 +2,72 @@
 
 int main()
 {
-  struct my_struct
   {
-    long a;
-    double b;
-    char c;
-    unsigned long d;
-    //bool operator==(my_struct) = default; // c++20
-    bool operator==(my_struct rhs)
+    struct my_struct
     {
-      return (rhs.a == a)
-        && (rhs.b == b)
-        && (rhs.c == c)
-        && (rhs.d == d);
+      long a;
+      double b;
+      char c;
+      unsigned long d;
+      //bool operator==(my_struct) = default; // c++20
+      bool operator==(my_struct rhs)
+      {
+        return (rhs.a == a)
+          && (rhs.b == b)
+          && (rhs.c == c)
+          && (rhs.d == d);
+      }
+    };
+
+    my_struct ref
+    {
+      .a = 3,
+      .b = 1,
+      .c = 4,
+      .d = 2,
+    };
+
+    {
+      my_struct entry = ref;
+      int rc = ipc_write(entry, "/test/topic");
+      assert(rc == 0);
     }
-  };
 
-  my_struct ref
-  {
-    .a = 3,
-    .b = 1,
-    .c = 4,
-    .d = 2,
-  };
+    {
+      my_struct entry = {0};
+      int rc = ipc_read(entry, "/test/topic");
+      assert(rc == 0);
+      assert(entry == ref);
+    }
+  }
 
   {
-    my_struct entry = ref;
-    int rc = ipc_write(entry, "/test/topic");
+    int rc = system(R"(python -c "
+from pytyndall import ipc_write_float, ipc_read_float
+rc = ipc_write_float(42, '/test/pytopic')
+assert rc == 0, 'write failed'
+
+f = ipc_read_float('/test/pytopic')
+assert f == 42, 'got different value back'
+    ")");
     assert(rc == 0);
   }
 
   {
-    my_struct entry = {0};
-    int rc = ipc_read(entry, "/test/topic");
-    assert(rc == 0);
-    assert(entry == ref);
+    {
+      int rc = system(R"(python -c "
+from pytyndall import ipc_write_float, ipc_read_float
+rc = ipc_write_float(66, '/test/py2c++topic')
+assert rc == 0, 'write failed'
+    ")");
+      assert(rc == 0);
+    }
+    {
+      float entry;
+      int rc = ipc_read(entry, "/test/py2c++topic");
+      assert(rc == 0);
+      assert(entry == 66);
+    }
   }
 
 }
--- tests/pytyndall_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from pytyndall import ipc_write_float, ipc_read_float
-from time import sleep
-
-rc = ipc_write_float(42, "/test/topic")
-assert rc == 0, "write failed"
-
-f = ipc_read_float("/test/topic")
-assert f == 42, "got different value back"
