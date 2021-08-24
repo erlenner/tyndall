36448fa3ec18473626133b46109706247da323e5
Erlend Eriksen <erlend_ne@hotmail.com>
2021-08-24 19:42:11 +0200

    reflection: fix recursion

--- tyndall/reflect/reflection.h
+++ tyndall/reflect/reflection.h
@@ -5,21 +5,6 @@ template<typename... Args>
 struct reflection
 {};
 
-namespace detail
-{
-  template<int index, typename Lhs, typename... Rhs, typename = std::enable_if_t<index == 0>>
-  static constexpr const Lhs& reflect_get(const reflection<Lhs, Rhs...>& refl) noexcept
-  {
-    return refl.lhs;
-  }
-  
-  template<int index, typename Lhs, typename... Rhs, typename = std::enable_if_t<0 < index>>
-  static constexpr auto reflect_get(const reflection<Lhs, Rhs...>& refl) noexcept -> decltype(reflect_get<index-1>(static_cast<const reflection<Rhs...>&>(refl)))
-  {
-    return reflect_get<index-1>(static_cast<const reflection<Rhs...>&>(refl));
-  }
-}
-
 template<typename Lhs, typename... Rhs>
 struct reflection<Lhs, Rhs...> : public reflection<Rhs...>
 {
@@ -33,7 +18,7 @@ struct reflection<Lhs, Rhs...> : public reflection<Rhs...>
   template<int index>
   constexpr auto get() noexcept
   {
-    return detail::reflect_get<index>(*this);
+    return get_impl<index>(*this);
   }
 
   constexpr int size() noexcept
@@ -43,6 +28,17 @@ struct reflection<Lhs, Rhs...> : public reflection<Rhs...>
 
 protected:
 
+  template<int index, typename = std::enable_if_t<index == 0>>
+  static constexpr const Lhs& get_impl(const reflection<Lhs, Rhs...>& refl) noexcept
+  {
+    return refl.lhs;
+  }
+
+  template<int index, typename = std::enable_if_t<0 < index>>
+  static constexpr auto get_impl(const reflection<Lhs, Rhs...>& refl) noexcept -> decltype(reflection<Rhs...>::template get_impl<index-1>(static_cast<const reflection<Rhs...>&>(refl)))
+  {
+    return reflection<Rhs...>::template get_impl<index-1>(static_cast<const reflection<Rhs...>&>(refl));
+  }
 };
 
 template<>
@@ -51,6 +47,12 @@ struct reflection<>
 public:
   explicit constexpr reflection() noexcept
   {}
+
+  template<int index>
+  static constexpr int get_impl(const reflection<>& refl) noexcept
+  {
+    return -1;
+  }
 
   constexpr int size() noexcept
   {
