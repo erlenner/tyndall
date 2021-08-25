361c48abb74b2a9b3acda0931f8e5fa2599335e8
Erlend Eriksen <erlend_ne@hotmail.com>
2021-08-25 18:49:42 +0200

    n_fields work

--- tests/reflect/n_fields.cpp
+++ tests/reflect/n_fields.cpp
@@ -1,5 +1,7 @@
 #include <tyndall/reflect/n_fields.h>
 
+#include <string>
+
 int main()
 {
   {
@@ -8,11 +10,13 @@ int main()
       float floating_point = 3.14f;
       int integer = 42;
       unsigned char unsigned_char = 255;
+      std::string ss;
     };
 
-    constexpr S s;
+    //constexpr S s{1, 2, 3};
+    S s;
 
-    //static_assert(n_fields<decltype(s)>() == 3);
+    static_assert(n_fields<decltype(s)>() == 4);
   }
 
 }
--- tyndall/reflect/n_fields.h
+++ tyndall/reflect/n_fields.h
@@ -1,23 +1,86 @@
 #include <type_traits>
+#include <utility>
 #include <climits>
-#include <stddef.h>
+#include <typeinfo>
+#include <cstdio>
 
-//template<size_t size>
-//using size_tc = std::integral_constant<size_t, size>;
+// heavy borrowing from https://github.com/apolukhin/pfr_non_boost/blob/master/include/pfr/detail/fields_count.hpp
 
-template<typename T, size_t N>
-constexpr std::enable_if_t<std::is_array_v<T>
-, std::size_t> n_fields_impl() noexcept
+namespace detail
 {
-  return sizeof(T) / sizeof(std::remove_all_extents_t<T>);
+  template<int... I>
+  using index_sequence = std::integer_sequence<int, I...>;
+
+  template<int N>
+  using make_index_sequence = std::make_integer_sequence<int, N>;
+
+  template <class T>
+  constexpr T unsafe_declval() noexcept {
+      constexpr typename std::remove_reference_t<T>* ptr = 0;
+      return static_cast<T>(*ptr);
+  }
+
+  struct ubiq_lref_constructor {
+      int ignore;
+      template <class T> constexpr operator T&() const && noexcept {
+          return unsafe_declval<T&>();
+      }
+  
+      template <class T> constexpr operator T&() const & noexcept {
+          return unsafe_declval<T&>();
+      }
+  };
+
+  struct ubiq_rref_constructor {
+      int ignore;
+      template <class T> constexpr operator T() const && noexcept {
+          return unsafe_declval<T>();
+      }
+  };
+
+  template<typename T, int... I, class = typename std::enable_if_t<std::is_copy_constructible_v<T>>>
+  constexpr auto constructible(index_sequence<I...>) noexcept
+    -> typename std::add_pointer_t<decltype(T{ ubiq_lref_constructor{I}... })>;
+
+  template<typename T, int... I, class = typename std::enable_if_t<!std::is_copy_constructible_v<T>>>
+  constexpr auto constructible(index_sequence<I...>) noexcept
+    -> typename std::add_pointer_t<decltype(T{ ubiq_rref_constructor{I}... })>;
+
+  //template<typename T, int... I>
+  //constexpr auto constructible(index_sequence<I...>) noexcept
+  //  -> decltype(T{I...});
+
+  template <class T, int N, class = decltype( constructible<T>(make_index_sequence<N>()) ) >
+  using constructible_t = int;
+
+  //template<typename T, int N>
+  //constexpr std::enable_if_t<std::is_array_v<T>
+  //, int> n_fields_impl() noexcept
+  //{
+  //  return sizeof(T) / sizeof(std::remove_all_extents_t<T>);
+  //}
+
+  template<typename T, int N>
+  constexpr auto n_fields_impl() noexcept -> constructible_t<T, N>
+  {
+    return N;
+  }
+
+  //template<typename T, int N>
+  //constexpr auto n_fields_match() noexcept -> constructible_t<T, N-1>
+  //{
+  //  return N-1;
+  //}
+
 }
 
 template<typename T>
-constexpr size_t n_fields() noexcept
+constexpr int n_fields() noexcept
 {
-  constexpr size_t max_field_count = sizeof(T)*CHAR_BIT;
+  //constexpr int max_field_count = sizeof(T)*CHAR_BIT;
+  constexpr int max_field_count = 4;
 
-  using type = typename std::remove_cv<T>;
+  using type = std::remove_cvref_t<T>;
 
-  return n_fields_impl<type, max_field_count>();
+  return detail::n_fields_impl<type, max_field_count>();
 }
